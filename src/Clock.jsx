68439719764ed108c5815a4d8b917dc04020fd65
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-19 12:20:14 -0700

    Add interval clock

---
+++ src/Clock.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+export default function Clock() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter(count => count + 1)
+    }, 1000);
+  }, [])
+
+  return (
+    <p>{counter} seconds have passed.</p>
+  );
+}
